ed20ac92105428c9d8b2722fa5c990b541b405c5
NotChain <tntareaper@gmail.com>
2020-02-10 23:29:52 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import numpy as np
 import cv2 as cv
-#import matplotlib.pyplot as plt
-from sklearn.utils import shuffle
-from sklearn.model_selection import train_test_split
-from sklearn import svm
-from sklearn.metrics import accuracy_score
-import os
+import joblib
 
+# With these I just set some values for the HOG descriptor so it knows how to process the ROI I give it based on the red detection
+#if you wish to know more about what each parameter does, check HOGDESscriptor's documentation. In short, it just sets the size
+#of the chunks in which the ROI is processed.
 winSize = (128, 128)
 blockSize = (16, 16)
 blockStride = (8, 8)
@@ -23,23 +21,12 @@ signedGradients = True
 hog = cv.HOGDescriptor(winSize, blockSize, blockStride, cellSize, nbins, derivAperture, winSigma, histogramNormType,
                         L2HysThreshold, gammaCorrection, nlevels, signedGradients)
 #------------ THE HOG DESCRIPTOR VALUES HAVE BEEN SET
-X = []
-Y = []
 
-def readFile(name, kind):
-    for file_type in [name]:
-        for imageName in os.listdir(file_type):
-            path = str(file_type) + "/" + str(imageName)
-            img = cv.imread(path)
-            descriptor = hog.compute(img)
-            descriptor = np.array(descriptor)
-            descriptor = descriptor.transpose()
-            print(len(descriptor[0]))
-            print("-------------")
-            X.append(descriptor[0])
-            Y.append(kind)
-
+#This is used  to detect all the red in the image and it returns the MASK (i.e. the RED is 1 and everything else is 0).
 def detectRed(img):
+    #This code is more or less off a neat tutorial. Mostly used that since I wasn't certain what range to pick for red.
+    #If this doesn't dettect all ranges, I can just modify the values so it takes more "colors" into account, since that will just
+    #decrease the performance a bit, but it will most likely not matter whatsoever.
     imgHSV = cv.cvtColor(img, cv.COLOR_BGR2HSV)
     # Range for lower red #Might as well let someone else pick the values
     lower_red = np.array([0, 120, 70])
@@ -51,6 +38,9 @@ def detectRed(img):
     mask2 = cv.inRange(imgHSV, lower_red, upper_red)
     return (mask1+mask2)
 
+#Here I use the "findContours" function to find the countours of the MASK I get from detecting RED. Then I follow this up by
+#calculating its "moments" (https://www.quora.com/What-exactly-are-moments-in-OpenCV for an explanation of what they are).
+#With said moments we calculate were the ROI should roughly be and then, using the SVM with HOG we test it.
 def getCenters(img):
     finalMask = detectRed(img)
     kernel = np.ones((5, 5), np.uint8)
@@ -71,161 +61,73 @@ def getCenters(img):
 
     return mc
 
+#Checking to see if the X coordinate is not out of image's bounds.
 def withinBoundsX(coord, img):
     if(coord < 0 or coord > img.shape[1]):
         return 0
     return 1
-
+#Analogous with the above, but for Y.
 def withinBoundsY(coord, img):
     if(coord < 0 or coord > img.shape[0]):
         return 0
     return 1
 
+#This is where the magic happens. We declare each points based on the previously found moments (we found the center with said moments).
+#Since the HoG uses 128x128 images, we make it so the center remains in the center by adding/subtracting 64 to the right coordinates.
 def detectSign(img, watch, mc):
     for points in mc:
         x1 = int(points[0]) - 64
         y1 = int(points[1]) - 64
         x2 = int(points[0]) + 64
         y2 = int(points[1]) + 64
-        #Check if it's within bounds
-        #watch = cv.pyrDown(watch)
-        #watch = cv.copyMakeBorder(watch, 0, 0, 0, 64, cv.BORDER_CONSTANT)
-        #cv.imshow("pain", watch)
-        #cv.rectangle(watch, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
+        #This is a bit rough. Through trial and error I figured that there's a chance the ROI is just a bit below/above the
+        #correct position, so I make 2 other ROI's, once 16 pixels below and one 16 pixels above. So we check 3 times for each
+        #detected countour center.
         for disp in range(-16, 16+1, 16):
             if (not withinBoundsX(x1, img) or  not withinBoundsY(y1+disp, img) or not withinBoundsX(x2, img) or not withinBoundsY(y2+disp, img)):
                 break
+            #The window to check using the HOG.
             roi = img[y1+disp:y2+disp, x1:x2]
-            #cv.imshow("roi", roi)
+
+            #Computing the HOG and making it so we can give it to the SVM.
             descriptor = hog.compute(roi)
             descriptor = np.array(descriptor)
             descriptor = descriptor.transpose()
+            #If the SVM gives a positive response (i.e. it is a stop sign) we show the image and draw a rectangle around the sign.
             if clf.predict(descriptor) == 1:
                 cv.rectangle(watch, (x1, y1+disp), (x2, y2+disp), (255, 0, 0), 2)
                 cv.imshow("pain", watch)
                 cv.waitKey(0)
-                # cv.imwrite("mining/mined" + str(index) + ".png", roi)
 
-readFile("signs", 1)
-readFile("negatives", -1)
-X = np.array(X)
-Y = np.array(Y)
-print(X)
-print(Y)
-X, Y = shuffle(X, Y)
-clf = svm.SVC(kernel = "linear", gamma = "auto")
-clf.fit(X, Y) #USING THE WHOLE DATASET
-#------------------THE SVM HAS BEEN TRAINED
-
-#------------------Reading image
-mining = cv.imread("test24.png")
-watch = cv.imread("test24.png")
-modify = cv.imread("test24.png")
-
-#------------------COUNTOUR PLAY
-'''detectRed(watch)
-mc = getCenters(watch)
-print(len(mc))
-for points in mc:
-    x1 = int(points[0]) - 64
-    y1 = int(points[1]) - 64
-    x2 = int(points[0]) + 64
-    y2 = int(points[1]) + 64
-    # Check if it's within bounds
-    # watch = cv.pyrDown(watch)
-    #watch = cv.copyMakeBorder(watch, 0, 0, 0, 64, cv.BORDER_REPLICATE)
-    # cv.imshow("pain", watch)
-    cv.rectangle(watch, (x1, y1), (x2, y2), (0, 0, 255), 2)
-    for disp in range(-16, 16+1, 16):
-        if (not withinBoundsX(x1, watch) or not withinBoundsY(y1+disp, watch) or not withinBoundsX(x2, watch) or not withinBoundsY(y2+disp, watch)):
-            break
-
-        roi = watch[y1+disp:y2+disp, x1:x2]
-        cv.imshow("roi", roi)
-        print(disp)
-        #cv.rectangle(watch, (x1, y1+disp), (x2, y2+disp), (0, 0, 255), 2)
-        # cv.imshow("roi", roi)
-        descriptor = hog.compute(roi)
-        descriptor = np.array(descriptor)
-        descriptor = descriptor.transpose()
-        if clf.predict(descriptor) == 1:
-            cv.rectangle(watch, (x1, y1), (x2, y2), (255, 0, 0), 2)
-            cv.imshow("pain", watch)
-            cv.waitKey(0)'''
-#cv.imshow("watch", watch)
-#---------------COUNTOUR PLAY UP!!! ^ ^ ^
-
-#detectSign(watch, watch, mc)
+#I load the SVM that I've trained in a separate piece of code. This has been made using 30 positives and roughly 100 negatives.
+#It is a linear SVM that can only classify whether something's a STOP SIGN or NOT.
+#A simple way to imagine it is as such: you have two separate data points clusters, the SVM draws a line between the two
+#thus dividing them into two categories. Whenever a new datapoint is added, it is given a category based on its position.
+#Note: This is horribly undersimplified.
+clf = joblib.load("stopSignLSVM.joblib")
 
 #----------------VIDEO TESTING WITH FUNCTIONS
-#----------------------VIDEO TESTING BEGINS
 cap = cv.VideoCapture('Set2/picam19-02-14-07-39n.h264')
 #Detect RED -> calclulate HoG -> give to SVM
 while(cap.isOpened()):
+    #Reading from the camera.
     ret, watch = cap.read()
     #watch = cv.pyrDown(watch)
+    #This I added because in some cases the stop sign is too much to the right so we get an image out of bounds error,
+    #so I just made the image a bit longer. A rough solution, but it works.
     watch = cv.copyMakeBorder(watch, 0, 0, 0, 128, cv.BORDER_REPLICATE)
+    #The image is scaled down so it detects the stop sign at the right time.
+    watch = cv.pyrDown(watch)
+    #Get the centers.
     mc = getCenters(watch)
+    #Detect the sign.
     detectSign(watch, watch, mc)
+    #Show the image.
     cv.imshow("pain", watch)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 
-
-#-------------------THIS IS JUST MINING
-'''index = 99
-for y in range(1, mining.shape[0] - 128, 10):
-    for x in range(1, mining.shape[1] - 128, 10):
-        roi = mining[y:y+128, x:x+128]
-        descriptor = hog.compute(roi)
-        descriptor = np.array(descriptor)
-        descriptor = descriptor.transpose()
-        if clf.predict(descriptor) == 1:
-            cv.rectangle(watch, (x, y), (x + 128, y + 128), (255, 0, 0), 2)
-            cv.imshow("pain", watch)
-            cv.waitKey(10)
-            #cv.imwrite("mining/mined" + str(index) + ".png", roi)
-            index+=1
-#cv.imshow("pain", testImg)
-cv.waitKey(0)'''
-#---------------------MINING ENDS HERE, DID FUCKING WONDERS
-
-#----------------------VIDEO TESTING BEGINS
-'''cap = cv.VideoCapture('Set2/picam19-02-14-07-39n.h264')
-#Detect RED -> calclulate HoG -> give to SVM
-while(cap.isOpened()):
-    ret, testImg= cap.read()
-    watch = testImg
-    for y in range(1,  int(testImg.shape[0]/2) - 128 - 128, 64):
-        for x in range(int(testImg.shape[1]/1.5), testImg.shape[1] - 128, 64):
-            #cv.rectangle(testImg, (x, y), (x + 128, y + 128), (255, 0, 0), 2)
-            roi = testImg[y:y+128, x:x+128]
-
-            maskFin= detectRed(roi)
-            whitePixels = cv.countNonZero(maskFin)
-            cv.rectangle(watch, (x, y), (x + 128, y + 128), (255, 255, 0), 2)
-            #print(whitePixels)
-            #if whitePixels > 4000:
-                #cv.imshow("White!", maskFin)
-                #cv.waitKey(0)
-            if 1:
-                descriptor = hog.compute(roi)
-                descriptor = np.array(descriptor)
-                descriptor = descriptor.transpose()
-                if clf.predict(descriptor) == 1:
-                    cv.rectangle(testImg, (x, y), (x + 128, y + 128), (0, 255, 0), 2)
-                    #cv.imshow("lol", roi)
-                    cv.imshow("pain", watch)
-                    cv.waitKey(1)
-                else:
-                    cv.imshow("pain", maskFin)
-                    pass
-            else:
-                cv.imshow("pain", watch)
-                pass
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cv.imshow("pain", testImg)'''
 cv.waitKey(0)
+cap.release()
+cv.destroyAllWindows()
